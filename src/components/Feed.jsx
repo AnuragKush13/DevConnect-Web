8248ff644b7ec8909ed10b7dca85c9ae60d60089
AnuragKush13 <anurag.kush13@gmail.com>
2024-10-05 00:50:31 +0530

    handled unauthorized user to route to login page directly

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constant'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../features/userSlice';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 const Feed = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const fetchUser = async()=>{
+        try{
+       const res = await axios.get(BASE_URL+"/profile/view",{withCredentials:true})
+       dispatch(addUser(res.data));
+        }
+        catch(err){
+            if(err.status === 401)
+            navigate("/login")
+            console.error(err)
+        }
+    }
+    useEffect(()=>{
+        fetchUser();
+    })
   return (
     <div>Feed</div>
   )
